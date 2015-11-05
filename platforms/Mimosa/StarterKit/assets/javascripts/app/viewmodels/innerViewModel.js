f8bdde9fc9beaa431bb3e7cbe89ab956ca09b3cf
Zabolai Zsolt <z.zsolti@yahoo.com>
2015-11-05 14:54:37 +0200

    other changes (seems that build.ps was not run recently)

---
+++ platforms/Mimosa/StarterKit/assets/javascripts/app/viewmodels/innerViewModel.js
@@ -0,0 +1,19 @@
+﻿define(function () {
+    var attachedCount = 0;
+
+    var InnerViewModel = function () {
+
+    };
+
+    InnerViewModel.prototype.attached = function () {
+        attachedCount++;
+        console.log("InnerViewModel.attached - attachedCount:" + attachedCount);
+    };
+
+    InnerViewModel.prototype.detached = function () {
+        attachedCount--;
+        console.log("InnerViewModel.detached - attachedCount:" + attachedCount);
+    };
+
+    return InnerViewModel;
+});
